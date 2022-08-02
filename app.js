446bd2dcc19f66ea8d310907d209b0b06692c136
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-08-02 21:11:42 +0000

    feat: basic drag & drop functionality

---
+++ app.js
@@ -0,0 +1,42 @@
+let p = document.getElementsByTagName('p');
+let choice = document.getElementsByClassName('choice');
+let dragItem = null;
+
+for(let i of p){
+    i.addEventListener('dragstart', dragStart);
+    i.addEventListener('dragend', dragEnd);
+}
+
+function dragStart(){
+    dragItem = this;
+    setTimeout(() => this.style.display = "none", 0);
+}
+
+function dragEnd(){
+    setTimeout(() => this.style.display = "block", 0);
+    dragItem = null;
+}
+
+for(j of choice){
+    j.addEventListener('dragover', dragOver);
+    j.addEventListener('dragenter', dragEnter);
+    j.addEventListener('dragleave', dragLeave);
+    j.addEventListener('drop', Drop);
+}
+
+function Drop(){
+    this.append(dragItem);
+}
+
+function dragOver(e){
+    e.preventDefault();
+    this.style.border = "2px dotted cyan";
+}
+
+function dragEnter(e){
+    e.preventDefault();
+}
+
+function dragLeave(){
+    this.style.border = "none";
+}
